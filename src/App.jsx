cc5e2769c20a3bdca36ef73a3b262bfd94881767
dit10z <yudithfabillah@gmail.com>
2024-08-13 18:48:48 +0700

    feat: sign-in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Pages from './pages'; 
-import ProTip from './components/ProTip';
-import Copyright from './components/Copyright';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Pages from "./pages";
+import ProTip from "./components/ProTip";
+import Copyright from "./components/Copyright";
 
 export default function App() {
   return (
     <Router>
-      <Container maxWidth="lg">
-        <Box sx={{ my: 4 }}>
-          <Pages /> {/* Ini akan mengatur semua routing dan halaman */}
-          <ProTip />
-          <Copyright />
-        </Box>
-      </Container>
+      {/* <Container maxWidth="lg"> */}
+      {/* <Box sx={{ my: 4 }}> */}
+      <Pages /> {/* Ini akan mengatur semua routing dan halaman */}
+      {/* <ProTip /> */}
+      {/* <Copyright /> */}
+      {/* </Box> */}
+      {/* </Container> */}
     </Router>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Administrators from "./Administators";
-import Attendances from "./Attendances";
 import Auth from "./Auth";
 import Companies from "./Companies";
 import Dashboard from "./Dashboard";
-import Departments from "./Departments";
-import Employees from "./Employees";
-import Holidays from "./Holidays";
-import Leaves from "./Leaves";
-import Settings from "./Settings";
 import PrivateRoute from "./PrivateRoute"; // Import PrivateRoute
-import { AdminSidebar, SuperadminSidebar } from "../components/Navigation";
+import { SuperadminSidebar } from "../components/Navigation";
 
 const Pages = () => {
   // Dummy variables for authentication and role
@@ -24,10 +18,6 @@ const Pages = () => {
       {isAuthenticated && userRole === "superadmin" && (
         // <div>Superadmin Sidebar</div> // Replace with actual Sidebar component for Superadmin
         <SuperadminSidebar />
-      )}
-      {isAuthenticated && userRole === "admin" && (
-        // <div>Admin Sidebar</div> // Replace with actual Sidebar component for Admin
-        <AdminSidebar />
       )}
 
       <Routes>
@@ -52,58 +42,10 @@ const Pages = () => {
           }
         />
         <Route
-          path="/attendances"
-          element={
-            <PrivateRoute isAuthenticated={isAuthenticated}>
-              <Attendances />
-            </PrivateRoute>
-          }
-        />
-        <Route
           path="/companies"
           element={
             <PrivateRoute isAuthenticated={isAuthenticated}>
               <Companies />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/departments/*"
-          element={
-            <PrivateRoute isAuthenticated={isAuthenticated}>
-              <Departments />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/employees/*"
-          element={
-            <PrivateRoute isAuthenticated={isAuthenticated}>
-              <Employees />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/holidays"
-          element={
-            <PrivateRoute isAuthenticated={isAuthenticated}>
-              <Holidays />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/leaves"
-          element={
-            <PrivateRoute isAuthenticated={isAuthenticated}>
-              <Leaves />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <PrivateRoute isAuthenticated={isAuthenticated}>
-              <Settings />
             </PrivateRoute>
           }
         />
