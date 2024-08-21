166b8ebf0c117d16a7277105d06a5a387468e970
dit10z <yudithfabillah@gmail.com>
2024-08-21 18:21:22 +0700

    feat: add screen dashboard with react-chartjs

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React from "react";
+import { Grid, Container, Box } from "@mui/material";
+import StatCard from "../../components/Card/StatCard";
+import CompanyOverview from "../../components/Card/CompanyOverview";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Container maxWidth="lg">
+      <Grid container spacing={4}>
+        <Grid item xs={12} sm={4}>
+          <StatCard
+            title="Total Company"
+            value="560"
+            update="+12%"
+            date="July 16, 2023"
+          />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <StatCard
+            title="Total Admin"
+            value="1050"
+            update="+5%"
+            date="July 14, 2023"
+          />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <StatCard
+            title="Total Employee"
+            value="250"
+            update="+12%"
+            date="July 10, 2023"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Box>
+            <CompanyOverview />
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Dashboard;
