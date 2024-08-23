941639a71dd3ab8250d0f88353ff0994c2f6aa6c
annisa_mufidah.s <annisams.student@gmail.com>
2024-08-23 16:46:34 +0700

    fix: integrasi add new company dan integrasi dashboard

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import adminReducer from "./slices/adminsSlice";
 import authReducer from "./slices/authSlice";
 import companyReducer from "./slices/companySlice";
+import dashboardReducer from "./slices/dashboardSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     admin: adminReducer,
     companies: companyReducer,
+    dashboard: dashboardReducer,
   },
 });
 
